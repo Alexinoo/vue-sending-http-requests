bc63c1c0e51c3b396c5ea1d987ba333362e22f89
Alex Mwangi <alex@stl-horizon.com>
2022-06-08 17:21:13 +0300

    Showing a Loading... message

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -43,10 +43,18 @@
 
 -We can use mounted() hook which is triggered / called when this exact component gets mounted
 
+-->
 
 
 
+<!-- Showing a Loading... Message if we are still loading Data
+============================================
 
+-Add isLoading data property and initialize it to false
+
+-Set it to true before fetching  false once the data is received
+
+-Show a paragraph ( Loading... ) if isLoading data property is true ; Otherwise show the ul once the data is received
 
 -->
 
@@ -59,13 +67,10 @@
       <div>
         <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
-      <ul>
-        <survey-result
-          v-for="result in results"
-          :key="result.id"
-          :name="result.name"
-          :rating="result.rating"
-        ></survey-result>
+      <p v-if="isLoading">Loading...</p>
+      <ul v-else>
+        <survey-result v-for="result in results" :key="result.id" :name="result.name" :rating="result.rating">
+        </survey-result>
       </ul>
     </base-card>
   </section>
@@ -83,13 +88,16 @@ export default {
   data(){
 
     return {
-      results : []
+      results : [] ,
+      isLoading : false,
     }
   },
 
   methods : {
 
     loadExperiences(){
+    
+        this.loading = true
 
         fetch('https://vue-http-demo-97b72-default-rtdb.firebaseio.com/surveys.json')
         .then(response =>{
@@ -101,6 +109,8 @@ export default {
           }          
         })
         .then( data => {
+
+          this.loading = false
           
           const newResults = [];
 
