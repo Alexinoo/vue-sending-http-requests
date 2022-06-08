caa379840023f0f08bce0b2122c3500c1bd62bd4
Alex Mwangi <alex@stl-horizon.com>
2022-06-08 15:39:09 +0300

    Imported axios and VueAxios from axios/vue-axios packages

--- src/main.js
+++ src/main.js
@@ -4,9 +4,14 @@ import BaseCard from './components/UI/BaseCard.vue';
 import BaseButton from './components/UI/BaseButton.vue';
 import App from './App.vue';
 
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 const app = createApp(App);
 
 app.component('base-card', BaseCard);
 app.component('base-button', BaseButton);
+
+app.use(VueAxios, axios);
 
 app.mount('#app');
